8f7d02d55aaa7c70631806dea2ca06502b6e8d02
Nana3792 <andrianasielli@outlook.com.gr>
2025-08-11 18:43:12 +0300

    Update config.py

--- fhir_proxy/app/config.py
+++ fhir_proxy/app/config.py
@@ -1,10 +1,14 @@
 from starlette.config import Config
-from starlette.datastructures import Secret
 
 config = Config(".env")
 
-PROXY_TIMEOUT = config("PROXY_TIMEOUT", cast=float, default=30.0)
+# Arborist Configuration
 ARBORIST_URL = config("ARBORIST_URL", default="")
 ARBORIST_TIMEOUT = config("ARBORIST_TIMEOUT", cast=int, default=5)
+
+# FHIR Configuration
 HAPI_FHIR_URL = config("HAPI_FHIR_URL", default="http://localhost:8080")
 SECURITY_TAG_PREFIX = config("SECURITY_TAG_PREFIX", default="gen3|")
+
+# Proxy Configuration
+PROXY_TIMEOUT = config("PROXY_TIMEOUT", cast=float, default=30.0)
