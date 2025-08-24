647485b6de60c9725bd56ef014769e3825f08849
Nana3792 <andrianasielli@outlook.com.gr>
2025-08-24 23:23:06 +0300

    Update config.py

--- fhir_proxy/app/config.py
+++ fhir_proxy/app/config.py
@@ -2,13 +2,9 @@ from starlette.config import Config
 
 config = Config(".env")
 
-# Arborist Configuration
-ARBORIST_URL = config("ARBORIST_URL", default="")
+ARBORIST_URL = config("ARBORIST_URL", default="http://localhost:8081/auth/resources")
 ARBORIST_TIMEOUT = config("ARBORIST_TIMEOUT", cast=int, default=5)
-
-# FHIR Configuration
-HAPI_FHIR_URL = config("HAPI_FHIR_URL", default="http://localhost:8080")
+HAPI_FHIR_URL = config("HAPI_FHIR_URL", default="http://localhost:8080/fhir")
 SECURITY_TAG_PREFIX = config("SECURITY_TAG_PREFIX", default="gen3|")
-
-# Proxy Configuration
 PROXY_TIMEOUT = config("PROXY_TIMEOUT", cast=float, default=30.0)
+GEN3_USER_URL= config("GEN3_USER_URL", default="https://qa.planx-pla.net/user/user")
